64036031aaae3f630a23eb536c27e4b729f84257
Jonibek <jonibekqodirovv@gmail.com>
2026-05-26 16:28:03 +0300

    Second

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://davo-admin-flame.vercel.app/", "http://localhost:5173"],
+    credentials: true,
   }),
 );
 app.use(express.json({ limit: "10kb" }));
